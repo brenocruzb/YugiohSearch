1ca37bca457b31fc690b10b2cb4e444a66517a62
Breno Cruz <breno.cruz@paggcerto.com.br>
2019-04-18 14:59:17 -0300

    Levantamento isolado das classes

---
+++ app/src/main/java/br/com/yugiohsearch/DeckActivity.kt
@@ -0,0 +1,7 @@
+package br.com.yugiohsearch
+
+import androidx.appcompat.app.AppCompatActivity
+
+class DeckActivity: AppCompatActivity() {
+
+}
---
+++ app/src/main/java/br/com/yugiohsearch/DeckListFragment.kt
@@ -0,0 +1,13 @@
+package br.com.yugiohsearch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DeckListFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
